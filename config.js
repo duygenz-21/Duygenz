d32c893a5dbbfa0265256264d2bae30b84606dec
duygenz-21 <duygenz05@gmail.com>
2026-01-28 15:34:01 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+export const SECRET_CONFIG = {
+    SUPABASE_URL: 'https://uqchbponkvxkbdkpkgub.supabase.co', 
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxY2hicG9ua3Z4a2Jka3BrZ3ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkyNjIxMDYsImV4cCI6MjA4NDgzODEwNn0.9xkQlWLymaxd3pndmVUr5TGWdJYwT7lIXM993QKtF3Q', // Key của sếp
+    DEFAULT_API_KEY: '' // Để trống để người dùng tự nhập trong giao diện
+};
